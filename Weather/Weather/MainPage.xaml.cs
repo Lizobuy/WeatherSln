e85b9407edaae15e471b416697712ddc013887af
Lizobuya Danisa <ldanisa2@gmail.com>
2022-06-08 14:13:18 +0200

    Add project files.

---
+++ Weather/Weather/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherApp;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace Weather
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+        private async void GetWeatherData()
+        {
+            var data = await Permissions.CheckStatusAsync<Permissions.LocationWhenInUse>();
+            if (data != PermissionStatus.Granted)
+            {
+                var newdata = await Permissions.RequestAsync<Permissions.LocationWhenInUse>();
+            }
+            var location = await Geolocation.GetLocationAsync();
+            var latitude = location.Latitude;
+            var longitude = location.Longitude;
+
+
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Add("Accept", "application/json");
+            var response = await client.GetStringAsync("https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=61baddf70109e767dc03934d5aa825e6");
+            var weatherData = JsonConvert.DeserializeObject<OpenWeatherData>(response);
+        }
+}   }
+     
